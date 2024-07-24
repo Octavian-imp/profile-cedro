1d524795d9e4146dd7e3e0d8109fdefda96c92d4
Octavian_Jr <sirghi1979@mail.ru>
2024-07-24 17:18:49 +0300

    тестовое задание на tailwindcss

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,51 @@
-console.log(process.env.NODE_EN);
+const swiper = new Swiper(".swiper", {
+  freeMode: true,
+  breakpoints: {
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 8,
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 32,
+    },
+  },
+});
+
+const menuOpenBtnId = "#menuOverlay";
+const menuCloseBtnSelector = ".menuOverlay_header_closeBtn";
+const menuOverlaySelector = ".menuOverlay_container";
+const durMenuAnimation = 0.2;
+
+document.querySelector(menuOpenBtnId).addEventListener("click", () => {
+  const tl = gsap.timeline();
+
+  tl.to(menuOverlaySelector, {
+    display: "block",
+    duration: 0.01,
+  }).to(menuOverlaySelector, {
+    opacity: 1,
+    duration: durMenuAnimation,
+    onStart: () => {
+      document.querySelector(menuOverlaySelector).style.overflow = "auto";
+      document.querySelector("body").style.overflow = "hidden";
+    },
+  });
+});
+
+document.querySelector(menuCloseBtnSelector).addEventListener("click", () => {
+  const tl = gsap.timeline();
+  tl.to(menuOverlaySelector, {
+    opacity: 0,
+    duration: durMenuAnimation,
+    onStart: () => {
+      document
+        .querySelector(menuOverlaySelector)
+        .style.removeProperty("overflow");
+      document.querySelector("body").style.removeProperty("overflow");
+    },
+  }).to(menuOverlaySelector, {
+    display: "none",
+    duration: 0.01,
+  });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,52 @@
 module.exports = {
   content: ["app/scss/**/*.scss"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        screens: {
+          sm: "343px",
+          xl: "1205px",
+        },
+      },
+      screens: {
+        sm: "343px",
+        xl: "1205px",
+      },
+      fontFamily: {
+        times: `'Times New Roman', Times, serif`,
+        roboto: `"Roboto", sans-serif`,
+      },
+      colors: {
+        danger: "#eb5757",
+        "danger-light": "#c07474",
+        brown: "#c08d74",
+        gold: "#d0bc7a",
+        "orange-light": "#ea976f",
+        dark: "#21242e",
+        gray: "#f7f7f7",
+        "gray-dark": "#e4e4e4",
+      },
+      textColor: {
+        // dark: "rgba(0, 0, 0, 0.48)",
+      },
+      borderRadius: {
+        xs: "0.25rem",
+      },
+      borderColor: {
+        "primary-dark": "#e4e4e4",
+        primary: "#f7f7f7",
+      },
+      fontSize: {
+        "8xl": [
+          "5rem",
+          {
+            lineHeight: "5rem",
+            fontWeight: "500",
+          },
+        ],
+      },
+    },
   },
   plugins: [],
 };
